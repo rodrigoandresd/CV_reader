11cba7cdf627752fa005550e6756ba2ed22cf2f1
rodrigoandresd <rodrigoaduqueg@gmail.com>
2023-02-02 16:44:16 -0500

    first implementation nature automate.js Co-authored-by: David Perez <davidperez95@users.noreply.github.com> Co-authored-by: paosua86 <paosua86@users.noreply.github.com>

--- back-end/uploads/automate.js
+++ back-end/uploads/automate.js
@@ -2,7 +2,7 @@ import fs from "fs";
 import pdf from 'pdf-parse';
 import natural from 'natural';
 
-let dataBuffer = fs.readFileSync('./CV_2.pdf');
+let dataBuffer = fs.readFileSync('./CV_1.pdf');
 
 pdf(dataBuffer).then(function (data) {
     const content = data.text;
@@ -12,5 +12,43 @@ pdf(dataBuffer).then(function (data) {
 });
 
 async function processData (content) {
-    console.log(content);
+    const tokenizer = new natural.WordTokenizer();
+    // const sentece = new natural.SentenceTokenizer();
+    // const tokenizer_2 = new natural.WordPunctTokenizer();
+    const token_2 = (tokenizer.tokenize(content));
+
+    console.log(token_2);
+    // console.log(sentece.tokenize(content));
+    // console.log(tokenizer_2.tokenize(content));
+
+    let keys = ["NOMBRES", "APELLIDOS", "ESTADO", "TELEFONOS", "TITULO", "FECHA", "Email", "EXPERIENCE" ];
+    let values = {};
+
+    for (let i = 0; i < token_2.length; i++) {
+        if (keys.includes(token_2[i])) {
+            let currentKey = token_2[i];
+            let currentValue = "";
+            i++;
+            while (!keys.includes(token_2[i])) {
+                currentValue += token_2[i] + " ";
+                i++;
+                if (i === token_2.length) {
+                    break;
+                }
+            }
+            values[currentKey] = currentValue.trim();
+        }
+    }
+    // const jsonString = JSON.stringify(values);
+    // fs.writeFile('./demo.json', jsonString, (err) => {
+    //     if (err) {
+    //         console.log(err);
+    //     } else {
+    //         console.log('created');
+        
+    //     }
+    // })
+    console.log(values);
 };
+
+
