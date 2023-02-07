6217d68fd1d7d8602389a744b40d34610efbf448
rodrigoandresd <rodrigoaduqueg@gmail.com>
2023-02-07 15:45:03 -0500

    prefilled aveced 1.2

--- talanreader-web/src/app/pages/prefilled/prefilled.component.ts
+++ talanreader-web/src/app/pages/prefilled/prefilled.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import consultorData from '../../demo.json';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 interface Consultor {
   consultorEmail: String,
@@ -18,7 +19,36 @@ export class PrefilledComponent implements OnInit {
   formTitle = 'email: example@mail.com'
   constructor() { }
   consultors: Consultor[] = consultorData;
+
+  editarForm = new FormGroup({
+    name: new FormControl(''),
+    email: new FormControl(''),
+    phone: new FormControl(''),
+    skills: new FormControl(''),
+    education: new FormControl(''),
+    experience: new FormControl('')
+  })
+
   ngOnInit(): void {
+    let consultor = this.activerouter.snapshot.paramMap.get('id');
+    let token = this.getToken();
+
+this.api.getSinglePacient(paciteid).subscribe(data => {
+
+this.datosPaciente = data[0];
+
+this.editarForm.setValue({
+  'nombre': this.datosPaciente.Nombre,
+  'correo': this.datosPaciente.Correo,
+  'dni': this.datosPaciente.DNI,
+  'direccion': this.datosPaciente.Direccion,
+  'codigostal': this.datosPaciente.CodigoPostal,
+  'genero': this.datosPaciente.Genero,
+  'telefono': this.datosPaciente.Telefono,
+  'pacienteId': this.datosPaciente.PacienteId,
+  'fechaNacimiento': this.datosPaciente.FechaNacimiento
+})
+})
   }
 
 }
