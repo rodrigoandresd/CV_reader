7d3551d59b9d3f520dbc4d44a0fe5a2c62ed662b
David Perez <daperezcruz@gmail.com>
2023-02-07 16:44:23 -0500

    creating the list to show the consultor object

--- talanreader-web/src/app/pages/prefilled/prefilled.component.ts
+++ talanreader-web/src/app/pages/prefilled/prefilled.component.ts
@@ -20,35 +20,35 @@ export class PrefilledComponent implements OnInit {
   constructor() { }
   consultors: Consultor[] = consultorData;
 
-  editarForm = new FormGroup({
-    name: new FormControl(''),
-    email: new FormControl(''),
-    phone: new FormControl(''),
-    skills: new FormControl(''),
-    education: new FormControl(''),
-    experience: new FormControl('')
-  })
-
   ngOnInit(): void {
-    let consultor = this.activerouter.snapshot.paramMap.get('id');
-    let token = this.getToken();
 
-this.api.getSinglePacient(paciteid).subscribe(data => {
-
-this.datosPaciente = data[0];
-
-this.editarForm.setValue({
-  'nombre': this.datosPaciente.Nombre,
-  'correo': this.datosPaciente.Correo,
-  'dni': this.datosPaciente.DNI,
-  'direccion': this.datosPaciente.Direccion,
-  'codigostal': this.datosPaciente.CodigoPostal,
-  'genero': this.datosPaciente.Genero,
-  'telefono': this.datosPaciente.Telefono,
-  'pacienteId': this.datosPaciente.PacienteId,
-  'fechaNacimiento': this.datosPaciente.FechaNacimiento
-})
-})
   }
 
+//   editarForm = new FormGroup({
+//     name: new FormControl(''),
+//     email: new FormControl(''),
+//     phone: new FormControl(''),
+//     skills: new FormControl(''),
+//     education: new FormControl(''),
+//     experience: new FormControl('')
+//   })
+
+//   ngOnInit(): void {
+//     let consultor = this.activerouter.snapshot.paramMap.get('id');
+//     let token = this.getToken();
+
+// this.api.getSinglePacient(paciteid).subscribe(data => {
+
+// this.datosPaciente = data[0];
+
+// this.editarForm.setValue({
+//   'nombre': this.datosPaciente.Nombre,
+//   'correo': this.datosPaciente.Correo,
+//   'dni': this.datosPaciente.DNI,
+//   'direccion': this.datosPaciente.Direccion,
+//   'codigostal': this.datosPaciente.CodigoPostal,
+//   'genero': this.datosPaciente.Genero,
+//   'telefono': this.datosPaciente.Telefono,
+//   'pacienteId': this.datosPaciente.PacienteId,
+//   'fechaNacimiento': this.datosPaciente.FechaNacimiento
 }
