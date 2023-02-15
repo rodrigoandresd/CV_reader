95b162a16fe131a78a3098cd559c0ad20ec9ee49
paosua86 <paolasuarezb1986@gmail.com>
2023-02-15 13:51:04 -0500

    added getwork update to work on it

--- back-end/reader/get_work.js
+++ back-end/reader/get_work.js
@@ -3,71 +3,84 @@
 // @param {array} tokens - An array of strings to search for work experience.
 // @returns {object} - The consultant object with added work experience, or with a default message if none was found.
 
-function getWork (consultorObj, tokens) {
-
+function getWork(consultorObj, tokens) {
     // Define arrays of keys to look for in the tokens array.
     const workSectionKeys = [
-        "work",
-        "laboral",
-        "fields",
-        "job",
-        "experiencia"
+      "work",
+      "laboral",
+      "fields",
+      "job",
+      "experiencia"
     ];
 
     const workComplementKeys = [
-        "experience",
-        "experiencia",
-        "of",
-        "profesional",
-        "laboral"
+      "experience",
+      "experiencia",
+      "of",
+      "profesional",
+      "laboral"
     ];
 
     const workKeysStop = [
-        "education",
-        "educacion",
-        "academic",
-        "academica",
-        "habilidades",
-        "references",
-        "referencias",
-        "skills",
-        "habilidades",
-        "conocimientos",
-        "idiomas",
-        "languajes",
-        "cursos",
+      "education",
+      "educacion",
+      "academic",
+      "academica",
+      "habilidades",
+      "references",
+      "referencias",
+      "skills",
+      "habilidades",
+      "conocimientos",
+      "idiomas",
+      "languajes",
+      "cursos",
     ];
 
-    // Use a for loop to search for the work experience keywords in the tokens array.
+    const dateFormats = [
+      /\d{1,2}\/\d{4}/, // MM/YYYY
+      /\d{4}/ // YYYY
+    ];
+
     let found_work = false;
     for (let i = 0; i < tokens.length;) {
-        if (workSectionKeys.includes(tokens[i]) && (workComplementKeys.includes(tokens[i + 1]))) {
-            if (!found_work) {
-                // If the keywords are found and work experience has not been added yet, extract the work experience value and add it to the consultant object.
-                let workCurrentValue = "";
-                i++;
-                while (!workKeysStop.includes(tokens[i])) {
-                    workCurrentValue += tokens[i] + " ";
-                    i++;
-                    if (i === tokens.length || workKeysStop.includes(tokens[i])) {
-                        break;
-                    }
-                }
-                consultorObj['consultorWork'] = workCurrentValue.trim();
-                found_work = true;
+      if (workSectionKeys.includes(tokens[i]) && workComplementKeys.includes(tokens[i + 1])) {
+        if (!found_work) {
+          let sections = [];
+          let currentSection = { dates: [], info: '' };
+          i += 2;
+          while (!workKeysStop.includes(tokens[i]) && i < tokens.length) {
+            const token = tokens[i];
+            const date = dateFormats.find(format => format.test(token));
+            if (date) {
+              // Se encontró una fecha, agregar la sección actual al array y crear una nueva sección
+              currentSection.info = currentSection.info.trim();
+              sections.push(currentSection);
+              currentSection = { dates: [date.exec(token)[0]], info: '' };
             } else {
-                i++;
+              // No es una fecha, agregar el token a la información de la sección actual
+              currentSection.info += token + ' ';
             }
-        } else {
             i++;
+          }
+          // Agregar la última sección al array
+          currentSection.info = currentSection.info.trim();
+          sections.push(currentSection);
+          // Guardar el array de secciones en el objeto de consultor
+          consultorObj['consultorWork'] = sections;
+          found_work = true;
+        } else {
+          i++;
         }
+      } else {
+        i++;
+      }
     }
     // If no work experience was found, add a default message to the consultant object.
     if (!consultorObj.hasOwnProperty('consultorWork')) {
-        consultorObj['consultorWork'] = "Work experience not found";
+      consultorObj['consultorWork'] = "Work experience not found";
     }
-
     return consultorObj;
-};
+  }
 
 export { getWork };
