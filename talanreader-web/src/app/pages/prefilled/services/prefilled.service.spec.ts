9529860d6da49bbe6a7f0a4ed4f8359fcafa8d92
paosua86 <paolasuarezb1986@gmail.com>
2023-02-16 08:58:41 -0500

    comments added

--- talanreader-web/src/app/pages/prefilled/services/prefilled.service.spec.ts
+++ talanreader-web/src/app/pages/prefilled/services/prefilled.service.spec.ts
@@ -6,11 +6,14 @@ describe('PrefilledService', () => {
   let service: PrefilledService;
 
   beforeEach(() => {
+    // Configure the testing module
     TestBed.configureTestingModule({});
+    // Get an instance of the PrefilledService
     service = TestBed.inject(PrefilledService);
   });
 
   it('should be created', () => {
+    // Test that the service is created
     expect(service).toBeTruthy();
   });
 });
--- talanreader-web/src/main.ts
+++ talanreader-web/src/main.ts
@@ -1,13 +1,15 @@
+// Import required modules and environment configurations
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+// Enable production mode when in production environment
 if (environment.production) {
   enableProdMode();
 }
-
+// Bootstrap the Angular application by creating a new platform and running the AppModule
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
   .catch(err => console.error(err));
