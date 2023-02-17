5b85757fd9f71066546c762ab835374d0b044f1e
paosua86 <paolasuarezb1986@gmail.com>
2023-02-17 09:30:29 -0500

    work restored

--- back-end/reader/get_work.js
+++ back-end/reader/get_work.js
@@ -22,8 +22,8 @@ function getWork(consultorObj, tokens) {
   ];
 
   const workKeysStop = [
-    "education",
-    "educacion",
+    "workcation",
+    "workcacion",
     "academic",
     "academica",
     "habilidades",
@@ -38,53 +38,36 @@ function getWork(consultorObj, tokens) {
   ];
 
   const dateFormats = [
-    /\d{1,2}\/\d{4}/ // MM/YYYY
+    /\d{4}/ // YYYY
   ];
 
   let found_work = false;
-  let sections = [];
-  let currentSection = { dates: [], info: '' };
-
-  for (let i = 0; i < tokens.length; i++) {
-    const token = tokens[i];
-    const date = dateFormats.find(format => format.test(token));
-    if (date) {
-      // Found a date, get the 4 following tokens as the date range
-      const dateRange = tokens.slice(i+1, i+5).slice(0, 4);
-      currentSection.dates = dateRange;
-      i += 4;
-      // Add the section to the array and create a new one
-      currentSection.info = currentSection.info.trim();
-      sections.push(currentSection);
-      currentSection = { dates: [], info: '' };
-    } else if (workSectionKeys.includes(token) && workComplementKeys.includes(tokens[i + 1])) {
-      if (!found_work) {
-        found_work = true;
-        i += 2;
-      } else {
-        // Add the previous section to the array and create a new one
-        currentSection.info = currentSection.info.trim();
-        sections.push(currentSection);
-        currentSection = { dates: [], info: '' };
-        i += 2;
-      }
-    } else if (workKeysStop.includes(token)) {
-      // Add the last section to the array
-      currentSection.info = currentSection.info.trim();
-      sections.push(currentSection);
-      break;
-    } else {
-      // Not a date or work key, add the token to the current section's info
-      currentSection.info += token + ' ';
+    for (let i = 0; i < tokens.length;) {
+        if (workSectionKeys.includes(tokens[i]) && (workComplementKeys.includes(tokens[i + 1]) || tokens[i + 1] !== ' ')) {
+            if (!found_work) {
+                let workCurrentValue = "";
+                i++;
+                while (!workKeysStop.includes(tokens[i])) {
+                    workCurrentValue += tokens[i] + " ";
+                    i++;
+                    if (i === tokens.length || workKeysStop.includes(tokens[i])) {
+                        break;
+                    }
+                }
+                consultorObj['consultorWork'] = workCurrentValue.trim();
+                found_work = true;
+            } else {
+                i++;
+            }
+        } else {
+            i++;
+        }
     }
-  }
 
   // If no work experience was found, add a default message to the consultant object.
-  if (sections.length === 0) {
-    consultorObj['consultorWork'] = "Work experience not found";
-  } else {
-    consultorObj['consultorWork'] = sections;
-  }
+  if (!consultorObj.hasOwnProperty('consultorWork')) {
+    consultorObj['consultorWork'] = "Work not found";
+}
 
   return consultorObj;
 }
